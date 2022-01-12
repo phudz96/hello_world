e3cc4f5beb0d528c611565208326d3068c9029f2
phudz96 <phuphach96@gmail.com>
2022-01-12 21:48:12 +0700

    add all file

---
+++ app/src/main/java/com/example/examplekotlin/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.examplekotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.ContactsContract
+import android.util.Log
+import android.widget.Toast
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        print("hello_world")
+    }
+
+}
+
